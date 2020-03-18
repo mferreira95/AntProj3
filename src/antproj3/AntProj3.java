8463a3bcba602593b1d2e742aaf6ec6e1f09a4b6
mferreira95 <migasferreira@gmail.com>
2020-03-18 12:09:41 +0000

    first commit

---
+++ src/antproj3/AntProj3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antproj3;
+
+import antproj2.Mensagem;
+
+/**
+ *
+ * @author Miguel Ferreira
+ */
+public class AntProj3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world from antproj3");
+        
+        Mensagem m = new Mensagem();
+        System.out.println(m.getMessage());
+    }
+    
+}
